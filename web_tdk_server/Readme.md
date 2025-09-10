13362a7cf64c87836fe3f8478d2e3a820ea26796
อิลฮัม หะยีดอเล๊าะ <ihsaniah112@gmail.com>
2025-09-10 15:45:46 +0700

    back to besic

--- web_tdk_server/Readme.md
+++
@@ -1,3 +0,0 @@
-# Web TDK Server
-
-This is the backend server for the Web TDK project. It is built using FastAPI.
--- web_tdk_server/schema/announcement.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class Announcement(BaseModel):
-    title: str
-    content: str
-
-class AnnouncementUpdate(BaseModel):
-    title: str = None
-    content: str = None
